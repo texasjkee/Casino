0cd14fb18999af3935a05e8bc95db9a98fd927d3
Michael <gt.gluck@gmail.com>
2024-04-14 16:37:54 +0300

    add: userApi

---
+++ client/src/entities/_user/_user.ts
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { API_PUBLIC_KEY, API_URL } from '../../app/store/network'
+import { IRequestRegisterApi, IResponseRegisterApi } from './types'
+
+export const _userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL
+  }),
+  endpoints: builder => ({
+    login: builder.mutation<any, any>({
+      query: body => ({
+        url: '/login',
+        method: 'POST',
+        body
+      })
+    }),
+    register: builder.mutation<IResponseRegisterApi, IRequestRegisterApi>({
+      query: body => ({
+        url: `${API_PUBLIC_KEY}/register`,
+        method: 'POST',
+        body
+      }),
+      transformResponse: (response: IResponseRegisterApi) => {
+        localStorage.setItem('access_token', response.access_token)
+        localStorage.setItem('refresh_token', response.refresh_token)
+        localStorage.setItem('isAuth', 'true')
+
+        return response
+      }
+    })
+  })
+})
+
+export const { useLoginMutation, useRegisterMutation } = _userApi
